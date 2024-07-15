eb7b1c51609caa17125d98a77430fc7f70a28ff1
Laura Sandoval <Laura.Sandoval@lasp.colorado.edu>
2024-07-15 13:46:18 -0600

    add gsi

--- sds_data_manager/stacks/ialirt_database_stack.py
+++ sds_data_manager/stacks/ialirt_database_stack.py
@@ -11,50 +11,55 @@ from constructs import Construct
 
 
 class IAlirtDatabaseStack(Stack):
-    """Stack for creating database"""
+    """Stack for creating the IALIRT database."""
 
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
-        """Parameters
+        """Initialize the IALIRT database stack.
+
+        Parameters
         ----------
         scope : Construct
             The App object in which to create this Stack
         construct_id : str
             The ID (name) of the stack
+        **kwargs
+            Additional keyword arguments.
         """
         super().__init__(scope, construct_id, **kwargs)
 
-        # Add a DynamoDB table for the metadata
-        self.metadata_table = ddb.TableV2(
+        self.packet_data_table = ddb.Table(
             self,
-            "MetadataTable",
+            "PacketDataTable",
             table_name="imap-packetdata-table",
-            removal_policy=RemovalPolicy.DESTROY,  # Change to RemovalPolicy.RETAIN if you want to keep the table
-            point_in_time_recovery=True,
+            # Change to RemovalPolicy.RETAIN to keep the table after stack deletion.
+            removal_policy=RemovalPolicy.DESTROY,
+            # Restore data to any point in time within the last 35 days.
+            point_in_time_recovery=False,
+            # Partition key (PK) = filename.
             partition_key=ddb.Attribute(
-                name="PK",
+                name="packet_filename",
                 type=ddb.AttributeType.STRING,
-                # This is the partition key (PK) which is left as a generic string. For our purposes, this is
-                # going to be the filename alone without path. This gives uniqueness of files down to the second.
             ),
+            # Sort key (PK) = reset # + spacecraft time ugps.
             sort_key=ddb.Attribute(
-                name="SK",
+                name="sct_vtcw_reset#sct_vtcw",
                 type=ddb.AttributeType.STRING,
-                # This is a sort key (SK) and is a sortable field. For our purposes with vertical partitioning
-                # the most common SK is going to be the type of file (e.g. CR, PDS, L1b, etc.). This will be used
-                # to query for all files of a certain type using a global secondary index.
             ),
-            dynamo_stream=ddb.StreamViewType.NEW_IMAGE,
+            # Enable DynamoDB streams for real-time processing
+            stream=ddb.StreamViewType.NEW_IMAGE,
         )
 
-        # Add a GSI partitioned by creation date (e.g. 2025/12/25) and sorted by
-        # the full object creation timestamp (e.g. 2025-12-25T11:22:33)
-        # This GSI supports querying and sorting data that appeared during a specific timeframe
-        self.metadata_table.add_global_secondary_index(
-            index_name="relevant-date-index",
+        # GSI partitioned by packet length and
+        # sorted by sct_vtcw_reset#sct_vtcw
+        self.packet_data_table.add_global_secondary_index(
+            index_name="PacketLengthIndex",
             partition_key=ddb.Attribute(
-                name="applicable-date", type=ddb.AttributeType.STRING
+                name="packet_length", type=ddb.AttributeType.NUMBER
             ),
-            sort_key=ddb.Attribute(name="SK", type=ddb.AttributeType.STRING),
+            sort_key=ddb.Attribute(
+                name="sct_vtcw_reset#sct_vtcw", type=ddb.AttributeType.STRING
+            ),
+            # Specifies what keys to include.
             projection_type=ddb.ProjectionType.INCLUDE,
-            non_key_attributes=["first-packet-time", "last-packet-time"],
+            non_key_attributes=["packet_filename"],
         )
