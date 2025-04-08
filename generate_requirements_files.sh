948e851f0d9752014691e76d46035fc558e41c7c
Maxine Hartnett <maxine.hartnett@lasp.colorado.edu>
2025-04-08 15:03:05 -0600

    Remove print statements

--- generate_requirements_files.sh
+++ generate_requirements_files.sh
@@ -1,14 +1,7 @@
 #!/bin/bash
 
-echo "Generating database requirements file"
 poetry export -f requirements.txt --with layer-database -o lambda_layer/database/requirements.txt --without-urls
-
-echo "Generating processing requirements file"
 poetry export -f requirements.txt --with layer-processing -o lambda_layer/processing/requirements.txt --without-urls
-
-echo "Generating spice requirements file"
 poetry export -f requirements.txt --with layer-spice -o lambda_layer/spice/requirements.txt --without-urls
-
-touch test.txt
 
 echo "Requirements files generated successfully."
