cd57b4344cb1fa5b6307a5a53b7a5677549ce170
Luisa <luisa.coakley@lasp.colorado.edu>
2025-05-07 17:02:27 -0600

    fix loop for potential jobs

--- tests/lambda_endpoints/test_batch_starter.py
+++ tests/lambda_endpoints/test_batch_starter.py
@@ -769,6 +769,20 @@ def test_def_cadence_map_event(session):
         # Verify the function was not called
         assert mock_submit.call_count == 1
 
+def test_bulk_reprocessing_all(session, mock_urlopen, caplog):
+    """Tests ``lambda_handler`` when there is bulk reprocessing for all instruments."""
+    _populate_file_catalog(session)
+    # leave instrument, data_level and descriptor blank
+    events = {"reprocessing": True, "start_date": 20230101, "end_date": 20260101}
+    context = {"context": "sample_context"}
+    # Add instrument and try again
+    with patch.object(batch_starter, "try_to_submit_job") as mock_submit:
+        lambda_handler(events, context)
+    # There should be two jobs submitted, one for swe, one for lo based off the existing
+    # l0 files
+    assert mock_submit.call_count == 2
+
+
 ###### HELPER FUNCTION TESTS #######
 def test_determine_max_version(session):
     """Test the ``determine_job_version`` function."""
