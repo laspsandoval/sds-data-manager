f881ae042905bea20e4790119b90af8e445c4be8
Luisa Coakley <48064300+lacoak21@users.noreply.github.com>
2025-05-08 13:41:52 -0600

    Update tests/lambda_endpoints/conftest.py
    Co-authored-by: Tenzin Choedon <36522642+tech3371@users.noreply.github.com>

--- tests/lambda_endpoints/conftest.py
+++ tests/lambda_endpoints/conftest.py
@@ -322,7 +322,7 @@ def _populate_file_catalog(session):
             ),
         ),
         AncillaryFiles(
-            file_path="/path/to/imap_swe_l1b-in-flight-cal_20240105_20250104_v002.cdf",
+            file_path="/path/to/imap_swe_l1b-in-flight-cal_20240104_20240106_v002.cdf",
             instrument="swe",
             descriptor="l1b-in-flight-cal",
             start_date=datetime(2024, 1, 5),
