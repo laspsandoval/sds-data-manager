c904f8072b1b4b4ab8f854870b575f753f850c3e
Luisa <luisa.coakley@lasp.colorado.edu>
2025-05-13 17:11:26 -0600

    remove api domain env var from batch starter

--- sds_data_manager/constructs/instrument_lambdas.py
+++ sds_data_manager/constructs/instrument_lambdas.py
@@ -28,7 +28,6 @@ class BatchStarterLambda(Construct):
         vpc: ec2.Vpc,
         sqs_queue: sqs.Queue,
         layers: list,
-        api_domain: str,
         **kwargs,
     ):
         """BatchStarterLambda Constructor.
@@ -55,8 +54,6 @@ class BatchStarterLambda(Construct):
             A FIFO queue to trigger the lambda with.
         layers : list
             List of Lambda layers cdk.cdfnOutput names.
-        api_domain : str
-            Domain for creating an api request url.
         kwargs : dict
             Keyword arguments
 
@@ -70,7 +67,6 @@ class BatchStarterLambda(Construct):
             "SECRET_NAME": rds_construct.rds_creds.secret_name,
             "ACCOUNT": f"{env.account}",
             "REGION": f"{env.region}",
-            "IMAP_DATA_ACCESS_URL": f"https://{api_domain}",
         }
         # Lambda should use private subnet with routes to NAT gateway to make
         # calls to IMAP_DATA_ACCESS_URL and get back responses.
