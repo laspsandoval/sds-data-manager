aa9aa231f0b617a1ae4cf4098c7bea29a7cc4836
Petty Dev Tigris <akaumeheiwa@menloinnovations.com>
2025-09-11 11:31:43 -0400

    Added helper text in the scheduled_jobs_config.csv, refactored tests to use tmp_path fixture

--- tests/infrastructure/test_scheduled_job_config_reader.py
+++ tests/infrastructure/test_scheduled_job_config_reader.py
@@ -1,9 +1,7 @@
 """Tests scheduled job config reader."""
 
 import re
-import tempfile
 import textwrap
-from pathlib import Path
 from unittest.mock import patch
 
 import pytest
@@ -11,98 +9,92 @@ import pytest
 from sds_data_manager.constructs import scheduled_job_config_reader
 
 
-def test_scheduled_job_config_reader():
+def test_scheduled_job_config_reader(tmp_path):
     """Tests the config reader."""
-    with tempfile.TemporaryDirectory() as temp_dir:
-        config_path = Path(temp_dir) / "test_scheduled_jobs_config.csv"
-        config = textwrap.dedent("""\
-        # this is a comment
-        cron(20 6 * * ? *), glows, l3b, descriptor1
+    config_path = tmp_path / "test_scheduled_jobs_config.csv"
+    config = textwrap.dedent("""\
+    # this is a comment
+    cron(20 6 * * ? *), glows, l3b, descriptor1
 
-        cron(20 14 * * ? *), glows, l3c, descriptor2
-        cron(20 14 * * ? *), glows, l3d, descriptor3
-        """)
+    cron(20 14 * * ? *), glows, l3c, descriptor2
+    cron(20 14 * * ? *), glows, l3d, descriptor3
+    """)
 
-        config_path.write_text(config, encoding="utf-8")
-        with patch.object(scheduled_job_config_reader, "CONFIG_PATH", new=config_path):
-            scheduled_jobs = scheduled_job_config_reader.read_scheduled_job_config()
-            assert scheduled_jobs == {
-                "cron(20 6 * * ? *)": [
-                    {
-                        "data_source": "glows",
-                        "data_type": "l3b",
-                        "descriptor": "descriptor1",
-                    },
-                ],
-                "cron(20 14 * * ? *)": [
-                    {
-                        "data_source": "glows",
-                        "data_type": "l3c",
-                        "descriptor": "descriptor2",
-                    },
-                    {
-                        "data_source": "glows",
-                        "data_type": "l3d",
-                        "descriptor": "descriptor3",
-                    },
-                ],
-            }
+    config_path.write_text(config, encoding="utf-8")
+    with patch.object(scheduled_job_config_reader, "CONFIG_PATH", new=config_path):
+        scheduled_jobs = scheduled_job_config_reader.read_scheduled_job_config()
+        assert scheduled_jobs == {
+            "cron(20 6 * * ? *)": [
+                {
+                    "data_source": "glows",
+                    "data_type": "l3b",
+                    "descriptor": "descriptor1",
+                },
+            ],
+            "cron(20 14 * * ? *)": [
+                {
+                    "data_source": "glows",
+                    "data_type": "l3c",
+                    "descriptor": "descriptor2",
+                },
+                {
+                    "data_source": "glows",
+                    "data_type": "l3d",
+                    "descriptor": "descriptor3",
+                },
+            ],
+        }
 
 
-def test_read_scheduled_job_config_throws_exception_for_unknown_instrument():
+def test_read_scheduled_job_config_throws_exception_for_unknown_instrument(tmp_path):
     """Tests config reader.
 
     Asserts that the config reader throws an
     exception with an unrecognized instrument.
     """
-    with tempfile.TemporaryDirectory() as temp_dir:
-        config_path = Path(temp_dir) / "test_scheduled_jobs_config.csv"
-        config = textwrap.dedent("""\
-                cron(20 6 * * ? *), swapeeee, l3b, descriptor1
-                """)
+    config_path = tmp_path / "test_scheduled_jobs_config.csv"
+    config = textwrap.dedent("""\
+            cron(20 6 * * ? *), swapeeee, l3b, descriptor1
+            """)
 
-        config_path.write_text(config, encoding="utf-8")
-        with patch.object(scheduled_job_config_reader, "CONFIG_PATH", new=config_path):
-            with pytest.raises(ValueError, match="Invalid instrument: swapeeee"):
-                scheduled_job_config_reader.read_scheduled_job_config()
+    config_path.write_text(config, encoding="utf-8")
+    with patch.object(scheduled_job_config_reader, "CONFIG_PATH", new=config_path):
+        with pytest.raises(ValueError, match="Invalid instrument: swapeeee"):
+            scheduled_job_config_reader.read_scheduled_job_config()
 
 
-def test_read_scheduled_job_config_throws_exception_for_wrong_number_of_fields():
+def test_read_scheduled_job_config_throws_exception_for_incorrect_fields(tmp_path):
     """Tests config reader.
 
     Asserts that the config reader throws an
     exception with a wrong number of fields.
     """
-    with tempfile.TemporaryDirectory() as temp_dir:
-        config_path = Path(temp_dir) / "test_scheduled_jobs_config.csv"
+    config_path = tmp_path / "test_scheduled_jobs_config.csv"
 
-        cases = ["swapi, l3b, sci\n", "cron(20 6 * * ? *), swapi, l3b, sci, extra\n"]
+    cases = ["swapi, l3b, sci\n", "cron(20 6 * * ? *), swapi, l3b, sci, extra\n"]
 
-        for case in cases:
-            config_path.write_text(case, encoding="utf-8")
-            with patch.object(
-                scheduled_job_config_reader, "CONFIG_PATH", new=config_path
-            ):
-                message = (
-                    "Each scheduled job should have ['schedule', 'instrument', "
-                    f"'data_level', 'descriptor']\nCurrent line: {case}"
-                )
-                with pytest.raises(ValueError, match=re.escape(message)):
-                    scheduled_job_config_reader.read_scheduled_job_config()
+    for case in cases:
+        config_path.write_text(case, encoding="utf-8")
+        with patch.object(scheduled_job_config_reader, "CONFIG_PATH", new=config_path):
+            message = (
+                "Each scheduled job should have ['schedule', 'instrument', "
+                f"'data_level', 'descriptor']\nCurrent line: {case}"
+            )
+            with pytest.raises(ValueError, match=re.escape(message)):
+                scheduled_job_config_reader.read_scheduled_job_config()
 
 
-def test_read_scheduled_job_config_throws_exception_for_unknown_data_level():
+def test_read_scheduled_job_config_throws_exception_for_unknown_data_level(tmp_path):
     """Tests config reader.
 
     Asserts that the config reader throws an
     exception with an unrecognized data_level.
     """
-    with tempfile.TemporaryDirectory() as temp_dir:
-        config_path = Path(temp_dir) / "test_scheduled_jobs_config.csv"
-        config = "cron(20 6 * * ? *), swapi, l3bc, sci\n"
+    config_path = tmp_path / "test_scheduled_jobs_config.csv"
+    config = "cron(20 6 * * ? *), swapi, l3bc, sci\n"
 
-        config_path.write_text(config, encoding="utf-8")
+    config_path.write_text(config, encoding="utf-8")
 
-        with patch.object(scheduled_job_config_reader, "CONFIG_PATH", new=config_path):
-            with pytest.raises(ValueError, match="Invalid data level: l3bc"):
-                scheduled_job_config_reader.read_scheduled_job_config()
+    with patch.object(scheduled_job_config_reader, "CONFIG_PATH", new=config_path):
+        with pytest.raises(ValueError, match="Invalid data level: l3bc"):
+            scheduled_job_config_reader.read_scheduled_job_config()
