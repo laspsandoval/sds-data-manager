8b8e23c65effdb6bd42dc808723a9ced0ea78e66
Laura Sandoval <46567335+laspsandoval@users.noreply.github.com>
2026-01-19 13:52:27 -0700

    I-ALiRT - update logs (#1067)

--- sds_data_manager/lambda_code/IAlirtCode/ialirt_ingest.py
+++ sds_data_manager/lambda_code/IAlirtCode/ialirt_ingest.py
@@ -362,7 +362,7 @@ def process_algorithms(  # noqa: PLR0915
                 logger.info("Processing HIT.")
                 result = process_func(combined)
 
-            logger.info("%s result: %s", instrument, result)
+            logger.info("[%s] results populated for [%s]", len(result), instrument)
 
             if any(result) and all(result):
                 if table_name == "ialirt-algorithm-table":
@@ -680,7 +680,7 @@ def lambda_handler(event, context):
 
     if filenames:
         logger.info("Found %d files to process", len(filenames))
-        logger.info("Parsing packets.")
+        logger.info(f"Parsing packet files: {filenames}")
         # Get packets into datasets and combine.
         combined = parse_packets(filenames, bucket, Path("/tmp"))  # noqa: S108
         logger.info("Packets parsed. Processing algorithms.")
