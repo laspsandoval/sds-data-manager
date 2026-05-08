461a0938ac93e588556f020c36a1a0a681be8b73
Luisa Coakley <48064300+lacoak21@users.noreply.github.com>
2026-05-08 07:07:57 -0600

    Create IDEX l0 db table (#1312)
    * add idex table
    
    * update idex table schema
    
    * add migration file
    
    * add version to primary key
    
    * update comment
    
    * comment update

--- sds_data_manager/lambda_code/SDSCode/database/models.py
+++ sds_data_manager/lambda_code/SDSCode/database/models.py
@@ -314,3 +314,20 @@ class Version(Base):
     # Data version is a string of the form vXXX
     data_version = Column(String(4), nullable=False)
     updated_date = Column(DateTime, nullable=False)
+
+
+class IDEXL0Files(Base):
+    """Idex l0 table."""
+
+    __tablename__ = "idex_l0_files"
+    # Set the primary key to be the combination of file path, start date and version.
+    # The combo of the three should always be unique.
+    file_path = Column(String, nullable=False, primary_key=True)
+    # IDEX CDF files l1a onward are organized in 10 day chunks. The start_date column
+    # refers to the beginning of that 10-day period.
+    # There can be more than one row for each file_path if it contains data that are
+    # in two different ten day chunks. These start_date are defined by IDEX team due to
+    # above reasons.
+    start_date = Column(DateTime, nullable=False, primary_key=True)
+    version = Column(String(2), nullable=False, primary_key=True)
+    ingestion_date = Column(DateTime(timezone=True))
